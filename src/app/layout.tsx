d4d59dee2a05012819c51947f3a42ce98df25555
Sarvam Patel <148036885+CoreTech7704@users.noreply.github.com>
2026-07-09 09:27:27 +0530

    update: integrate @vercel/speed-insights for performance monitoring and enhance layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -94,6 +95,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full bg-[#030712] text-white overflow-x-hidden flex flex-col">{children}</body>
+      <SpeedInsights />
     </html>
   );
 }
